0148e915f0813a412fd03bd42fca5a0979a826b5
BDanyil <d.baltaksa@gmail.com>
2021-03-04 20:28:10 +0200

    Add task title validation

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:title) }
+    it { should validate_length_of(:title).is_at_most(140) }
   end
 
   describe 'associations' do
